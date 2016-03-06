53bc150866fba217fc3c6f75b423972adb003f8b
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-06 13:35:09 -0400

    getting from view, putting data from server.

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -1,16 +1,18 @@
 package masterSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
 
     @RequestMapping("/")
-    public String index(){
+    public String index(@RequestParam("name") String name,  Model model){
+        model.addAttribute("message", "Hello "+name+" from the controller");
         return "resultPage";
     }
-
 
 }
