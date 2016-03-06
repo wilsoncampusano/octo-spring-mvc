ad0d295de4ce045b3a19e1016b8142e736613264
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-06 14:15:53 -0400

    tweets.

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -1,20 +1,33 @@
 package masterSpringMvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Objects;
+
 @Controller
 public class HelloController {
 
+    @Autowired
+    private Twitter twitter;
 
     @RequestMapping("/")
     public String index(@RequestParam(value = "name",
                                     required = false, defaultValue = "")
                             String name,  Model model){
 
-        model.addAttribute("message", "Hello "+name+" from the controller");
+        SearchResults searchResults = twitter.searchOperations().search(name);
+        Tweet tweet = searchResults.getTweets().get(0);
+        if(Objects.isNull(tweet))
+            model.addAttribute("message", "No results");
+        else
+            model.addAttribute("message", tweet.getText());
         return "resultPage";
     }
 
