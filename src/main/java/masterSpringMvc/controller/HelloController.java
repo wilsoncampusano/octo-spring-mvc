0448084d28f61c3906d7b1aa4ea45dee4775edc3
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-06 12:41:20 -0400

    calling a thymeleaf view

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -2,15 +2,14 @@ package masterSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
 
     @RequestMapping("/")
-    public @ResponseBody String index(){
-        return "Hola Mundo";
+    public String index(){
+        return "resultPage";
     }
 
 
