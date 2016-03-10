829fd34eb3be4bf13ed7f77083c51f0510bc6a9d
Wilson Campusano <wicampusano@arshumano.com>
2016-03-10 09:04:41 -0400

    post request.

--- src/main/java/masterSpringMvc/controller/TwitterController.java
+++ src/main/java/masterSpringMvc/controller/TwitterController.java
@@ -8,8 +8,11 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +25,16 @@ public class TwitterController {
     @RequestMapping("/")
     public String index(){
         return "searchPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String SEARCH_KEY = "search";
+
+        String search = request.getParameter(SEARCH_KEY);
+        redirectAttributes.addAttribute(SEARCH_KEY, search);
+
+        return "redirect:result";
     }
 
     @RequestMapping("/result")
