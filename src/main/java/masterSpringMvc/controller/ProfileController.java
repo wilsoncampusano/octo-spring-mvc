0e28ae66bee889529f0dc9f2d9e3ced3b8f81e5f
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-12 15:52:11 -0400

    profile page and controller

---
+++ src/main/java/masterSpringMvc/controller/ProfileController.java
@@ -0,0 +1,15 @@
+package masterSpringMvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+
+
+    @RequestMapping("/profile")
+    public String displayProfile(){
+        return "profile/profilePage";
+    }
+
+}
