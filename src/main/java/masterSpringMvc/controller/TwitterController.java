c3592c74f1ae68f856deba3eda08ddf98c6dfcd2
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-12 15:23:35 -0400

    profile page

--- src/main/java/masterSpringMvc/controller/TwitterController.java
+++ src/main/java/masterSpringMvc/controller/TwitterController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class TwitterController {
