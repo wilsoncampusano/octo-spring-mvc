e64ca6f0f4a86b3fdeb66d2635dca85ba8f09c72
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-12 16:28:10 -0400

    date formatter configuration

---
+++ src/main/java/masterSpringMvc/controller/WebConfiguration.java
@@ -0,0 +1,18 @@
+package masterSpringMvc.controller;
+
+
+import masterSpringMvc.date.USLocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.time.LocalDate;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
+    }
+}
