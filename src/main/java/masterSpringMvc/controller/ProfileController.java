6d1d7328bddc79e810aabf721851bc6768a5ef67
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-03-13 13:43:49 -0400

    ProfileController : add taste , remove taste, taste empty html5 validation.

--- src/main/java/masterSpringMvc/controller/ProfileController.java
+++ src/main/java/masterSpringMvc/controller/ProfileController.java
@@ -1,12 +1,14 @@
 package masterSpringMvc.controller;
 
 import masterSpringMvc.date.USLocalDateFormatter;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -24,6 +26,19 @@ public class ProfileController {
         return "profile/profilePage";
     }
 
+    @RequestMapping(value = "/profile",method = RequestMethod.POST, params = "addTaste")
+    public String addTaste(ProfileForm profileForm){
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
+
+
+    @RequestMapping(value="/profile" , method = RequestMethod.POST, params = "removeTaste")
+    public String removeTaste(ProfileForm profileForm, HttpServletRequest request){
+        int index = Integer.valueOf(request.getParameter("removeTaste")).intValue();
+        profileForm.getTastes().remove(index);
+        return "/profile/profilePage";
+    }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
     public String saveProfile(@Valid  ProfileForm profileForm, BindingResult bindingResult){
